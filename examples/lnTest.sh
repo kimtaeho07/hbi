a903425b9dfc5bfbba967270499a1b56bcbcf253
Taeho Kim <kimtaeho@sh03-ln06.stanford.edu>
2026-01-20 11:33:58 -0800

    removed examples

--- examples/lnTest.sh
+++
@@ -1,8 +0,0 @@
-### Make output and back up directories 
-ODIR=/oak/stanford/groups/tchelepi/kimtaeho/Fervo/hbi/hbi_gaussianSource/pSig1
-if [ ! -e $ODIR ]; then
-    mkdir $ODIR 
-fi
-ln -s $ODIR output
-rm -r $ODIR/*
-
